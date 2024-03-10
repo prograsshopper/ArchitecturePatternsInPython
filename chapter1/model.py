5f2ed99278afb6f2d1a748ed44c3e72faca5531d
prograsshopper <16264413+prograsshopper@users.noreply.github.com>
2024-03-10 17:44:42 +0900

    chapter 1: 1.3 models for complicate process, value object, entity

--- chapter1/model.py
+++ chapter1/model.py
@@ -3,6 +3,14 @@ from typing import Optional
 
 from attr import dataclass
 
+'''
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: OrderReference
+    sku: ProductReference
+    qty: Quantity
+'''
+
 
 @dataclass(frozen=True)
 class OrderLine:
@@ -15,11 +23,34 @@ class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
         self.reference = ref
         self.sku = sku
-        self.available_quantity = qty
         self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations = set()
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
 
     def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine) -> bool:
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
+
