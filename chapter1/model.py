447b7dbb77f4d07df38388764ce307c77ebb0ec5
prograsshopper <16264413+prograsshopper@users.noreply.github.com>
2024-03-10 16:48:14 +0900

    chapter 1: ~1.3 first test and create Batch model

---
+++ chapter1/model.py
@@ -0,0 +1,25 @@
+from datetime import date
+from typing import Optional
+
+from attr import dataclass
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    order_id: str
+    sku: str
+    qty: int
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.available_quantity = qty
+        self.eta = eta
+
+    def allocate(self, line: OrderLine):
+        self.available_quantity -= line.qty
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
---
+++ chapter1/test_model.py
@@ -0,0 +1,36 @@
+from datetime import date, timedelta
+import pytest
+
+from model import OrderLine, Batch
+
+today = date.today()
+tomorrow = today + timedelta(days=1)
+later = tomorrow + timedelta(days=10)
+
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
+    line = OrderLine('order-ref', "SMALL-TABLE", 2)
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
+
+
+def test_can_allocate_if_available_greater_than_required():
+    pytest.fail("todo")
+
+
+def test_cannot_allocate_if_available_smaller_than_required():
+    pytest.fail("todo")
+
+
+def test_can_allocate_if_available_equal_to_required():
+    pytest.fail("todo")
+
+
+def test_prefers_warehouse_batches_to_shipments():
+    pytest.fail("todo")
+
+
+def test_prefers_earlier_batches():
+    pytest.fail("todo")
