3cc505d5cc6b74d5bf3262732656fb983c42f270
prograsshopper <16264413+prograsshopper@users.noreply.github.com>
2024-03-24 02:34:55 +0900

    chapter 4: 4.4 error case for monitoring DB

--- chapter4/original/flask_app.py
+++ chapter4/original/flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -13,6 +13,9 @@ orm.start_mappers()
 get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
 
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
@@ -22,6 +25,12 @@ def allocate_endpoint():
         request.json["orderid"], request.json["sku"], request.json["qty"],
     )
 
-    batch_ref = model.allocate(line, batches)
+    if not is_valid_sku(line.sku, batches):
+        return jsonify({'message': f'Invalid sku {line.sku}'}), 400
+
+    try:
+        batchref = model.allocate(line, batches)
+    except model.OutOfStock as e:
+        return {"message": str(e)}, 400
 
     return {"batchref": batchref}, 201
--- chapter4/original/test_api.py
+++ chapter4/original/test_api.py
@@ -67,3 +67,28 @@ def test_allocations_are_persisted(add_stock):
     assert r.json()["batchref"] == batch2
 
 
+@pytest.mark.usefixtures("restart_api")
+def test_400_message_for_out_of_stock(add_stock):
+    # 재고보다 더 많은 단위 할당 시도
+    sku, small_batch, large_order = random_sku(), random_batchref(), random_orderid()
+    add_stock([
+        (small_batch, sku, 10, '2011-01-01')
+    ])
+    data = {"orderid": large_order, "sku": sku, "qty": 20}
+    url = config.get_api_url()
+
+    r = requests.post(f"{url}/allocate", json=data)
+
+    assert r.status_code == 400
+    assert r.json()['message'] == f'Out of stock for sku {sku}'
+
+
+@pytest.mark.usefixtures("restart_api")
+def test_400_message_for_invalid_sku():
+    # sku가 존재하지 않는다 - out of stock 호출하지않음
+    unknown_sku, orderid = random_sku(), random_orderid()
+    data = {"orderid": orderid, "sku": unknown_sku, "qty": 20}
+    url = config.get_api_url()
+    r = requests.post(f"{url}/allocate", json=data)
+    assert r.status_code == 400
+    assert r.json()["message"] == f"Invalid sku {unknown_sku}"
