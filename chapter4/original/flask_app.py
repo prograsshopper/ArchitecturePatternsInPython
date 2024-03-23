0778d2d9d50dcef5dd0b2793bd40b83d9f679877
prograsshopper <16264413+prograsshopper@users.noreply.github.com>
2024-03-24 02:01:45 +0900

    chapter 4: ~4.3 non-commit api and api_test

---
+++ chapter4/original/flask_app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import config
+import model
+import orm
+import repository
+import services
+
+
+orm.start_mappers()
+get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
+app = Flask(__name__)
+
+
+@app.route("/allocate", methods=["POST"])
+def allocate_endpoint():
+    session = get_session()
+    batches = repository.SqlAlchemyRepository(session).list()
+    line = model.OrderLine(
+        request.json["orderid"], request.json["sku"], request.json["qty"],
+    )
+
+    batch_ref = model.allocate(line, batches)
+
+    return {"batchref": batchref}, 201
